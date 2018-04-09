4bf027c8e9a4e8837d336eebc13ffb82a9a7e7d1
Diego Noguera <danoguera@gmail.com>
2018-04-09 16:08:59 -0500

    Expenses per user added

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,5 +1,6 @@
 class ExpensesController < ApplicationController
+	before_action :authenticate_user!
   def index
-     @expenses = Expense.order("date DESC")
+     @expenses = Expense.order("date DESC").where("user_id=?",current_user.id)
   end
 end
